09b6dc71d0f014d20b0e4ce02fbfe8f5b45913be
Alexis Aranda Ocampo <alexis_aranda_ocampo@hotmail.com>
2019-06-06 18:55:49 -0300

    Cambios en cifrado

--- src/grain/Cypher.java
+++ src/grain/Cypher.java
@@ -2,16 +2,29 @@ package grain;
 
 public class Cypher {
 	private byte[] plaintext, keystream;
-  private KeystreamGenerator kGen;
+	private KeystreamGenerator kGen;
 
 	/**
 	 * 
 	 * @param key clave para cifrar
 	 * @param iv semilla para keystream
-   * @param plaintext texto a encriptar/desencriptar
+	 * @param plaintext texto a encriptar/desencriptar
 	 * @throws MuchosOPocosBytesException si key no tiene 80 bits o iv no tiene 64 bits
 	 */
-	public Cypher(byte[] key, byte[] iv, byte[] plaintext) {
-		
+	public Cypher(byte[] key, byte[] iv, byte[] plaintext) throws MuchosOPocosBytesException {
+		kGen = new KeystreamGenerator(key, iv);
+		this.plaintext = plaintext;
+		kGen.generarKeystream(this.plaintext.length);
 	}
+	
+	/**
+	 * Método que se encarga de realizar el cifrado
+	 * @return
+	 */
+	public byte[] cifrar() {
+		byte[] tCifrado;
+		//TODO cifrado
+		return tCifrado;
+	}
+	
 }
