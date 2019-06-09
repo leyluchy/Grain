8898b5da74e974313e569979b78df727d9ccb316
Alexis Aranda Ocampo <alexis_aranda_ocampo@hotmail.com>
2019-06-09 14:34:50 -0300

    Se implementa el ImageHandler

---
+++ src/grain/ImageHandler.java
@@ -0,0 +1,27 @@
+package grain;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class ImageHandler {
+	
+	public static byte[] imageToByteArray(BufferedImage originalImage) throws IOException{
+	ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	ImageIO.write( originalImage, "bmp", baos );
+	baos.flush();
+	byte[] imageInByte = baos.toByteArray();
+	baos.close();
+	return imageInByte;
+	}
+	
+	public static BufferedImage byteArrayToImage(byte[] originalArray) throws IOException{
+		ByteArrayInputStream bis = new ByteArrayInputStream(originalArray);
+	    BufferedImage image = ImageIO.read(bis);
+	    bis.close();
+	    return image;
+	}
+}
