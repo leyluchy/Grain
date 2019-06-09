f510da4f576d7f9376770f32bec4b29c203ba455
Alexis Aranda Ocampo <alexis_aranda_ocampo@hotmail.com>
2019-06-08 21:07:25 -0300

    Xoreo listo
    creo

--- src/grain/Cypher.java
+++ src/grain/Cypher.java
@@ -2,28 +2,44 @@ package grain;
 
 public class Cypher {
 	private byte[] plaintext, keystream;
+	private int largo;
 	private KeystreamGenerator kGen;
 
 	/**
-	 * 
+	 * Crea el Generador de claves y obtiene el keystream
 	 * @param key clave para cifrar
 	 * @param iv semilla para keystream
 	 * @param plaintext texto a encriptar/desencriptar
 	 * @throws MuchosOPocosBytesException si key no tiene 80 bits o iv no tiene 64 bits
 	 */
-	public Cypher(byte[] key, byte[] iv, byte[] plaintext) throws MuchosOPocosBytesException {
+	public Cypher(byte[] key, byte[] iv, byte[] plaintext) throws MuchosOPocosBytesException, LargosDiferentesException {
 		kGen = new KeystreamGenerator(key, iv);
 		this.plaintext = plaintext;
-		kGen.generarKeystream(this.plaintext.length);
+		this.keystream = kGen.generarKeystream(this.plaintext.length);
+		if((this.largo = this.plaintext.length)!=this.keystream.length)
+			throw new LargosDiferentesException();
 	}
 	
 	/**
-	 * MÃ©todo que se encarga de realizar el cifrado
-	 * @return
+	 * Método que se encarga de realizar el xoreo entre el texto plano y el keystream
+	 * @return array de bytes ya cifrados
 	 */
-	public byte[] cifrar() {
-		byte[] tCifrado;
-		//TODO cifrado
+	public byte[] getXored() {
+		byte[] tCifrado = this.xorArray(this.plaintext,this.keystream);
+		return tCifrado;
+	}
+	
+	/**
+	 * Método que se encarga de xorear elemento por elemento
+	 * @param x1 uno de las listas de bytes a xorear
+	 * @param x2 otra de las listas a de bytes a xorear
+	 * @return lista de elementos xoreados
+	 */
+	private byte[] xorArray(byte[] x1, byte[] x2) {
+		byte[] tCifrado = new byte[this.largo];
+		for (int i=0; i<this.largo; i++) {
+			tCifrado[i] = (byte) (this.plaintext[i] ^ this.keystream[i]);
+		}
 		return tCifrado;
 	}
 	
---
+++ src/grain/LargosDiferentesException.java
@@ -0,0 +1,10 @@
+package grain;
+
+public class LargosDiferentesException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
