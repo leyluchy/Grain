546ae1364d81a759f332cbb8d7406b54f6b783de
Alexis Aranda Ocampo <alexis_aranda_ocampo@hotmail.com>
2019-06-08 22:04:02 -0300

    Xoreo listo de nuevo
    creo, de nuevo

--- src/grain/Cypher.java
+++ src/grain/Cypher.java
@@ -13,9 +13,9 @@ public class Cypher {
 	 * @throws MuchosOPocosBytesException si key no tiene 80 bits o iv no tiene 64 bits
 	 */
 	public Cypher(byte[] key, byte[] iv, byte[] plaintext) throws MuchosOPocosBytesException, LargosDiferentesException {
-		kGen = new KeystreamGenerator(key, iv);
+		kGen = new KeystreamGenerator(ChadByteArrayTovirginShortArray(key), ChadByteArrayTovirginShortArray(iv));
 		this.plaintext = plaintext;
-		this.keystream = kGen.generarKeystream(this.plaintext.length);
+		this.keystream = virginShortArrayToChadByteArray(kGen.generarKeystream(this.plaintext.length*8)); //Se multiplica por el cambio de tipo
 		if((this.largo = this.plaintext.length)!=this.keystream.length)
 			throw new LargosDiferentesException();
 	}
@@ -43,4 +43,23 @@ public class Cypher {
 		return tCifrado;
 	}
 	
+	private byte[] virginShortArrayToChadByteArray(short[] v) {
+		byte[] salida = new byte[v.length/8];
+		for(int i=0;i<(v.length/8);i++) {
+			salida[i] = (byte) (v[i*8]*128+v[i*8+1]*64+v[i*8+2]*32+v[i*8+3]*16+v[i*8+4]*8+v[i*8+5]*4+v[i*8+6]*2+v[i*8+7]);
+		}
+		return salida;
+	}
+	
+	private short[] ChadByteArrayTovirginShortArray(byte[] v) {
+		short[] salida = new short[v.length*8];
+		for(int i=0;i<v.length;i++) {
+			byte aux = v[i];
+			for(int j=7;j>=0;j--) {
+				salida[i*8+j] = (short) (aux % 2);
+				aux = (byte) (aux / 2);
+			}
+		}
+		return salida;
+	}
 }
